10a93368491dbde61b4a0898ceb318e5dc1003d8
theGaryLarson <gary.larson316@gmail.com>
2023-04-25 23:31:00 -0700

    Refactored toast message to use data binding

--- app/src/main/java/com/example/kotlinplayground/MainActivity.kt
+++ app/src/main/java/com/example/kotlinplayground/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.kotlinplayground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.kotlinplayground.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil
+            .setContentView(this, R.layout.activity_main)
+
+        val factText = "BC <3 Gini"
+        val fictionText = "BC prefers cats over dogs"
+        val duration = Toast.LENGTH_LONG
+        binding.startButton.text = "Get Fact"
+        binding.endButton.text = "Get Fiction"
+        val factToast = Toast.makeText(applicationContext, factText, duration)
+        val fictionToast = Toast.makeText(applicationContext, fictionText, duration)
+
+        binding.startButton.setOnClickListener {
+            if (binding.startButton.text == "Get Fact") {
+                binding.startButton.text = "Get Fiction"
+                binding.endButton.text = "Get Fact"
+                factToast.show()
+
+            } else {
+                binding.startButton.text = "Get Fact"
+                binding.endButton.text = "Get Fiction"
+                fictionToast.show()
+
+            }
+        }
+
+        binding.endButton.setOnClickListener {
+            if (binding.endButton.text == "Get Fiction") {
+                binding.endButton.text = "Get Fact"
+                binding.startButton.text = "Get Fiction"
+                fictionToast.show()
+
+            } else {
+                binding.endButton.text = "Get Fiction"
+                binding.startButton.text = "Get Fact"
+                factToast.show()
+            }
+        }
     }
 }
